f748dfe647f3f547c10312ca671209ab270d948a
Arturas Milius <arturas114@gmail.com>
2021-01-01 17:04:37 +0200

    Initial Commit

---
+++ InfiniteScrollPlayground/InfiniteScrollPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteScrollPlaygroundApp.swift
+//  InfiniteScrollPlayground
+//
+//  Created by Arturas Milius on 2021-01-01.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteScrollPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
